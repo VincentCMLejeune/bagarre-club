fc2ad63aca465cc023e62cf8048e8102faaed4a7
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-02-11 12:20:30 +0100

    Improved character class

--- src/algorithmRefacto/CharacterClass.js
+++ src/algorithmRefacto/CharacterClass.js
@@ -5,9 +5,9 @@ export default class Fighter {
     this.intelligence = Number(intelligence) || 15;
     this.strength = Number(strength) || 15;
     this.speed = Number(speed) || 15;
-    this.initialSpeed = Number(speed) || 15;
+    this.initialSpeed = this.speed;
     this.life = Number(durability) || 15;
-    this.initialLife = Number(durability) || 15;
+    this.initialLife = this.life;
     this.power = Number(power) || 15;
     this.combat = Number(combat) || 15;
 
